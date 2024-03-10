98a5cfde1ea78dc795c35c6369ada0df156dc90d
thainq34 <nqthai03@gmail.com>
2024-03-10 23:19:34 +0700

    [Router] Fix api endpoint

--- src/actions/WeatherForecastAction.js
+++ src/actions/WeatherForecastAction.js
@@ -6,7 +6,7 @@ import {
 } from "../helpers/apiEndpoints";
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-const api = apiEndpointStaging; //you need to change this apiEndpoint to your real localhost
+const api = apiEndpointLocal; //you need to change this apiEndpoint to your real localhost
 export const getWeatherForecast = createAsyncThunk(
   "list/getWeatherForecast",
   async () => {
