640820472fa357683d12cc35660609609f9ef663
thainq34 <nqthai03@gmail.com>
2024-03-10 21:01:42 +0700

    [Init] Test Local host doesnt CICD

--- src/actions/WeatherForecastAction.js
+++ src/actions/WeatherForecastAction.js
@@ -6,7 +6,7 @@ import {
 } from "../helpers/apiEndpoints";
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-const api = apiEndpointStaging;
+const api = apiEndpointStaging; //you need to change this apiEndpoint to your real localhost
 export const getWeatherForecast = createAsyncThunk(
   "list/getWeatherForecast",
   async () => {
