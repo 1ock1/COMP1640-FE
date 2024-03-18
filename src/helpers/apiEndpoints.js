c3146799861fcac1198a4ca433f0a80e8a8a5239
Nguyen Vu Quang Minh <vuminh394@gmail.com>
2024-03-18 14:12:48 +0700

    Update on Faculty Page

--- src/helpers/apiEndpoints.js
+++ src/helpers/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const apiEndpointLocal = "https://localhost:7044/"; //or replace to your localhost
+export const apiEndpointLocal = "http://localhost:5134/"; //or replace to your localhost
 export const apiEndpointStaging = "https://comp1640-stg.azurewebsites.net/";
 export const apiEndpointProduction = "https://comp1640-prod.azurewebsites.net/";
 export const path = {
